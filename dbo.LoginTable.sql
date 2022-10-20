325b541a4f94c6141636e81e2c7f554db5a30b38
bryce96 <brycerr802@gmail.com>
2022-10-20 19:08:45 -0400

    Revert "Project Files"
    This reverts commit f9d7291afd54ea1c590744255a101f16414068f6.

--- dbo.LoginTable.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[LoginTable] (
-    [Username] VARCHAR (50) NOT NULL,
-    [Password] VARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_LoginTable] PRIMARY KEY CLUSTERED ([Username] ASC)
-);
-
